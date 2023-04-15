468ea868732589f7654bee13a0984277dc8c00a8
AlekseyKibyakov <lexa111lexa6@mail.ru>
2023-04-15 13:50:30 +0300

    add models templates, add urls parsing

--- models.py
+++ models.py
@@ -5,3 +5,35 @@ Base = declarative_base()
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
+
+
+class Message(Base):
+    __tablename__ = 'message'
+
+    
+    
+    def __str__(self):
+        return [
+            
+            ]
+
+
+class Author(Base):
+    __tablename__ = 'author'
+
+    
+    
+    def __str__(self):
+        return [
+            
+            ]
+
+class Tag(Base):
+    __tablename__ = 'tag'
+
+    
+    
+    def __str__(self):
+        return [
+            
+            ]
--- parsing_excel.py
+++ parsing_excel.py
@@ -1,22 +1,42 @@
-import pandas as pd
+from pprint import pprint
+# import pandas as pd
+import openpyxl as oxl
+import re
 
 file = 'MMH_RESPI_s_05.12.2022_13.04.2023-14.04.2023_643952c3ed42dd6e9d2b9572.xlsx'
 
-df_mentions = pd.read_excel(file, sheet_name='Упоминания', engine='openpyxl')
+wb = oxl.open(file)
+sh = wb.worksheets[1]
+sh.delete_cols(0)
+sh.delete_rows(1, 5)
 
-df_mentions = df_mentions.drop(columns='Unnamed: 0')
+urls_msgs = []
 
-titles_list = df_mentions.iloc[4].values.copy()
+url_re = r'.*\(\"{1}(.*)\"\){1}'
 
-df_mentions = df_mentions.drop([0, 1, 2, 3, 4])
+for row in range(2, 10):
+    urls_msgs.append(re.sub(url_re, r'\1', sh[row][5].value))
 
-df_mentions.columns = titles_list
+pprint(urls_msgs)
 
-df_mentions.reset_index(drop=True, inplace=True)
 
-df_mentions.to_excel('test.xlsx', engine='openpyxl')
+# df_mentions = pd.read_excel(file, sheet_name='Упоминания', engine='openpyxl')
 
-dates = dict(df_mentions.iloc[:, 0])
+# df_mentions = df_mentions.drop(columns='Unnamed: 0')
+
+# titles_list = df_mentions.iloc[4].values.copy()
+
+# df_mentions = df_mentions.drop([0, 1, 2, 3, 4])
+
+# df_mentions.columns = titles_list
+
+# df_mentions.reset_index(drop=True, inplace=True)
+
+# df_mentions.to_csv('test.csv', encoding='utf8')
+
+# urls = df_mentions.iloc[:, 4]
+
+# print(urls)
 
 # # Сброс ограничений на количество выводимых рядов
 # pd.set_option('display.max_rows', 50)
