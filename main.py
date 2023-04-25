ecc4d06e4dcd57175be761781b7bcafb680478fd
AlekseyKibyakov <lexa111lexa6@mail.ru>
2023-04-25 14:31:12 +0300

    fix schedule

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import parsing_excel as parse_xlsx
 
 
 def main():
-    # schedule.every().day.at('12:28').do(get_xlsx.run)
-    schedule.every().day.at('14:26').do(parse_xlsx.run)
+    schedule.every().day.at('00:20').do(get_xlsx.run)
+    schedule.every().day.at('01:30').do(parse_xlsx.run)
     while True:
         schedule.run_pending()
     
